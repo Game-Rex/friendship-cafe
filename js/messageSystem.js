5f90d003de2942ce7e73c731f5286db96ab47338
Game-Rex <an241401@gmail.com>
2026-08-02 12:21:22 +0530

    Create js/messageSystem.js

---
+++ js/messageSystem.js
@@ -0,0 +1,10 @@
+/**
+ * Message System
+ * Responsible for returning the unique Friendship Day message
+ * for a given food item.
+ */
+export default {
+  getMessage(foodId) {
+    return "";
+  },
+};
