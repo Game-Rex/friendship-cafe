f4409a1bbe6a5fcb24ec48c99217b1cbabf85c60
Game-Rex <an241401@gmail.com>
2026-08-02 12:19:13 +0530

    Create js/popupSystem.js

---
+++ js/popupSystem.js
@@ -0,0 +1,8 @@
+/**
+ * Popup System
+ * Responsible for showing the brief order-confirmation popup
+ * (e.g. "Excellent choice! One Pizza coming right up!").
+ */
+export default {
+  show(message) {},
+};
