91288fdceed74aa7883056985a709c936e4298ad
Game-Rex <an241401@gmail.com>
2026-08-02 12:18:18 +0530

    Create js/menuSystem.js

---
+++ js/menuSystem.js
@@ -0,0 +1,10 @@
+/**
+ * Menu System
+ * Responsible for opening/closing the food menu and reporting
+ * the visitor's selection back to the caller.
+ */
+export default {
+  open() {},
+  close() {},
+  onSelect(callback) {},
+};
