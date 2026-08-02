5a90f30905db4366877b0aae0c2150d9844a25d3
Game-Rex <an241401@gmail.com>
2026-08-02 12:20:25 +0530

    Create js/foodSystem.js

---
+++ js/foodSystem.js
@@ -0,0 +1,10 @@
+/**
+ * Food System
+ * Responsible for storing and returning data about each
+ * food/drink item (name, image, etc.).
+ */
+export default {
+  getFoodData(foodId) {
+    return null;
+  },
+};
