44de6e0a5400a4391c040e3fb15b2edfeb0b03d6
Game-Rex <an241401@gmail.com>
2026-08-02 12:22:31 +0530

    Create js/main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+import MenuSystem from './menuSystem.js';
+import PopupSystem from './popupSystem.js';
+import WaiterSystem from './waiterSystem.js';
+import FoodSystem from './foodSystem.js';
+import MessageSystem from './messageSystem.js';
+import AnimationSystem from './animationSystem.js';
+
+console.log('MenuSystem loaded', MenuSystem);
+console.log('PopupSystem loaded', PopupSystem);
+console.log('WaiterSystem loaded', WaiterSystem);
+console.log('FoodSystem loaded', FoodSystem);
+console.log('MessageSystem loaded', MessageSystem);
+console.log('AnimationSystem loaded', AnimationSystem);
